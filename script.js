8799cfa668c3c697e47d31c093fdd4cd5cfaa260
abhinavhomie01 <abhikr.6074@gmail.com>
2026-03-29 00:17:40 +0530

    displayed fetched details under profile section

--- script.js
+++ script.js
@@ -8,17 +8,36 @@ searchButton.addEventListener("click",async function (){
         return;
     }
     // console.log(username);
-
+    profileBox.innerHTML="<p>Loading...</p>"
     try{
     const response = await fetch (`https://api.github.com/users/${username}`);
     if(!response.status){
         throw new error("User not found")
     }
     const data =await response.json();
-    console.log(data);
+    // console.log(data);
+    profileBox.innerHTML=`
+    <img src="${data.avatar_url}" class="avatar" >
+    <h2>${data.name || "No name"}</h2>
+    <p>${data.bio || "No bio available"}</p>
+    <div class ="stats">
+        <div>
+            <strong>${data.followers}</strong>
+            <p>Followers</p>
+        </div>
+        <div>
+            <strong>${data.following}</strong>
+            <p>Following</p>
+            </div>
+        <div>
+        <strong>${data.public_repos}</strong>
+        <p>Repositories</p>
+        </div>
+    </div>`
 }
 catch(error){
-    console.log(error.message);
+    // console.log(error.message);
+    profileBox.innerHTML=`<p>style="color:red">User not found</p>`;
 }
 });
 
@@ -26,5 +45,6 @@ userInput.addEventListener("keypress",function(e){
     if(e.key==="Enter")
         searchButton.click();
 });
+const profileBox=document.getElementById("profileBox");
 
 
